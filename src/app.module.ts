1a87156f5e0ee102c97d3be91c1873b6d8596eac
Джабаров Саид <dsaidik123@gmail.com>
2026-03-18 08:40:26 +0300

    Lab-2 Complete

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,49 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { join } from 'path';
 
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { Program } from './programs/entities/program.entity';
+import { Shift } from './shifts/entities/shift.entity';
+import { Instructor } from './instructors/entities/instructor.entity';
+import { Participant } from './participants/entities/participant.entity';
+import { Application } from './applications/entities/application.entity';
+import { Review } from './reviews/entities/review.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        entities: [
+          Program,
+          Shift,
+          Instructor,
+          Participant,
+          Application,
+          Review,
+        ],
+        synchronize: true,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/reviews/entities/review.entity.ts
@@ -0,0 +1,38 @@
+﻿import {
+  Check,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Participant } from '../../participants/entities/participant.entity';
+import { Program } from '../../programs/entities/program.entity';
+
+@Entity('reviews')
+@Check(`rating >= 1 AND rating <= 5`)
+export class Review {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'int' })
+  rating: number;
+
+  @Column({ type: 'text' })
+  comment: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ManyToOne(() => Participant, (participant) => participant.reviews, {
+    nullable: false,
+    onDelete: 'CASCADE',
+  })
+  participant: Participant;
+
+  @ManyToOne(() => Program, (program) => program.reviews, {
+    nullable: false,
+    onDelete: 'CASCADE',
+  })
+  program: Program;
+}
